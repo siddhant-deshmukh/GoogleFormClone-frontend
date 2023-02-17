0d3588bdeb2eea14cd3f89785202930be2334deb
siddhant-deshmukh <siddhant946044@gmail.com>
2023-02-17 12:16:20 +0530

    Commit made from VS Code Azure Static Web Apps

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,47 @@ import TitleDescFormElement from './components/TitleDescFormElement';
 import QuestionFormElement from './components/QuestionFormElement';
 import { FormProvider } from './context/FormContext';
 import EditForm from './routes/EditForm';
+import { googleLogout, GoogleLogin, GoogleOAuthProvider, useGoogleLogin } from '@react-oauth/google';
 
 
 function App() {
-
+  const [googleLoginState,setGoogleLoginState] = useState<any>(null)
+  const [msg,setMsg] = useState(null)
+  const login = useGoogleLogin({
+    onSuccess : (res)=> {console.log("Sucessfully login!"); setGoogleLoginState(res)},
+    onError : (err) => {console.error("While login",err)}
+  })
+  useEffect(()=>{
+    if(googleLoginState){
+      fetch('https://google-clone-server-01.azurewebsites.net/',{method:'GET',mode: 'no-cors'})
+        .then((res)=>res.json())
+        .then((data)=>{console.log("Data from that link!!!",data), setMsg(data)})
+    }
+  },[googleLoginState])                                                          
   return (
     <>
-     <FormProvider>
+    
+      <FormProvider>
         <EditForm />
       </FormProvider> 
+      {/* <GoogleLogin  onError={()=>{console.error("Error in google login")}}/> */}
+      {
+        !googleLoginState &&
+        <button onClick={()=>{login()}}>Google Login</button>
+      }
+      {
+        googleLoginState &&
+        <button onClick={()=>{googleLogout(); setGoogleLoginState(null)}}>Logout</button>
+      }
+      {
+        googleLoginState &&
+        <div>{JSON.stringify(googleLoginState)}</div>
+      }
+      {
+        msg &&
+        <div>msg -- : <hr/><hr/>{JSON.stringify(msg)}</div>
+      }
+    
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
+import { GoogleOAuthProvider } from '@react-oauth/google'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <GoogleOAuthProvider clientId={import.meta.env.VITE_GoogleClientId}>
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+  </GoogleOAuthProvider>
 )
